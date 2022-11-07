cd36d7d22c4a322b2536363d76e6750054a72574
Mário Ferreira <marioo.netoo3@gmail.com>
2022-11-07 10:47:26 -0300

    começando
    Começo de um editor html básico, usando propriedades bem legais no csharp

---
+++ Menu.cs
@@ -0,0 +1,13 @@
+using System;
+
+public static class Menu
+{
+    public static void Show()
+    {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        
+    }
+}
